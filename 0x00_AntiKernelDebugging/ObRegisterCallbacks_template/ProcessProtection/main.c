8e955838636f85b39127d7c297e4397802b28f09
Shhoya_ <36813165+Shhoya@users.noreply.github.com>
2020-03-05 00:17:38 +0900

    Update main.c

--- 0x00_AntiKernelDebugging/ObRegisterCallbacks_template/ProcessProtection/main.c
+++ 0x00_AntiKernelDebugging/ObRegisterCallbacks_template/ProcessProtection/main.c
@@ -12,19 +12,20 @@ NTSTATUS ObRegExample()
 	OB_CALLBACK_REGISTRATION obRegistration = { 0, };
 	OB_OPERATION_REGISTRATION opRegistration = { 0, };
 
-	obRegistration.Version = ObGetFilterVersion();
-	obRegistration.OperationRegistrationCount = 1;
-	RtlInitUnicodeString(&obRegistration.Altitude, L"300000");
+	obRegistration.Version = ObGetFilterVersion();				// Get version
+	obRegistration.OperationRegistrationCount = 1;				// OB_OPERATION_REGISTRATION count, opRegistration[2] 인 경우 2
+	RtlInitUnicodeString(&obRegistration.Altitude, L"300000");	// 임의의 Altitude 지정
 	obRegistration.RegistrationContext = NULL;
 
 	opRegistration.ObjectType = PsProcessType;
-	opRegistration.Operations = OB_OPERATION_HANDLE_CREATE;
-	opRegistration.PreOperation = PreCallback;
-	opRegistration.PostOperation = PostCallback;
+	opRegistration.Operations = OB_OPERATION_HANDLE_CREATE;		// Create 또는 Open 시 동작
+	opRegistration.PreOperation = PreCallback;					// PreOperation 등록
+	opRegistration.PostOperation = PostCallback;				// PostOperation 등록
 
-	obRegistration.OperationRegistration = &opRegistration;
+	obRegistration.OperationRegistration = &opRegistration;		// OperationRegistration 등록
 	
 	DbgPrintEx(DPFLTR_ACPI_ID, 0, "[+] ObRegisterCallbacks Test\n");
+
 	return ObRegisterCallbacks(&obRegistration,&hRegistration);
 }
 
@@ -37,17 +38,21 @@ NTSTATUS DriverEntry(IN PDRIVER_OBJECT pDriver, IN PUNICODE_STRING pRegPath)
 {
 	UNREFERENCED_PARAMETER(pRegPath);
 	UNREFERENCED_PARAMETER(pDriver);
+
 	NTSTATUS ret = STATUS_SUCCESS;
 	DbgPrintEx(DPFLTR_ACPI_ID, 0, "[+] Load Driver\n");
-	pDriver->DriverUnload = UnloadDriver;
+
+	pDriver->DriverUnload = UnloadDriver;	// 언로드 루틴 등록
+
 	ret = ObRegExample();
+
 	if (ret==STATUS_SUCCESS)
 	{
 		DbgPrintEx(DPFLTR_ACPI_ID, 0, "[+] Success Registeration\n");
 	}
 	else
 	{
-		DbgPrintEx(DPFLTR_ACPI_ID, 0, "[+] Failed Registration %IX\n",ret);
+		DbgPrintEx(DPFLTR_ACPI_ID, 0, "[+] Failed Registration %X\n",ret);
 	}
 	return STATUS_SUCCESS;
 }
@@ -60,7 +65,8 @@ NTSTATUS DriverEntry(IN PDRIVER_OBJECT pDriver, IN PUNICODE_STRING pRegPath)
 VOID UnloadDriver(IN PDRIVER_OBJECT pDriver)
 {
 	UNREFERENCED_PARAMETER(pDriver);
-	if (hRegistration)
+
+	if (hRegistration)	// 콜백 등록에 실패할 경우 예외 처리
 	{
 		ObUnRegisterCallbacks(hRegistration);
 	}
